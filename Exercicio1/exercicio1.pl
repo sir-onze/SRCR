d073ac08820fcd0691e3b455a4b5f9d8c3cbd195
sir-onze <tiago96baptista@sapo.pt>
2019-03-02 15:28:10 +0000

    base2.0

--- Exercicio1/exercicio1.pl
+++ Exercicio1/exercicio1.pl
@@ -30,6 +30,31 @@ utente(28293,margaridaleao,20,braga).
 utente(31323,fredericasantos,20,beja).
 utente(33345,paulotabuada,20,sintra).
 utente(51523,adrianodovitoria20,aveiro).
+utente(11,tiagotombado,braga).
+utente(8,ricardosquirtle,braga).
+utente(69,lucasblastoise,braga).
+
 
 % - Serviços
+
+serviço(00001,cardiologia,hospital,braga).
+serviço(00002,enfermaria,hospital,braga).
+serviço(00003,psiquiatria,hospital,guimaraes).
+serviço(00004,podologia,centrodesaude,barcelos).
+serviço(00005,geneacologia,hospital,guimaraes).
+serviço(00001,cardiologia,clinica,barcelos).
+serviço(00007,dermatologia,clinicaprivada,porto).
+serviço(00002,enfermaria,centrodesaude,barcelos).
+serviço(00009,oftalmologia,hospital,guimaraes).
+serviço(00010,fisioterapia,hospital,porto).
+serviço(00011,ortopedia,clinicaprivada,barcelos).
+serviço(00011,enfermaria,hospital,porto).
+serviço(00002,enfermaria,centrodesaude,guimaraes).
+serviço(00014,clinicageral,centrodesaude,braga).
+serviço(00015,urgencia,hospitalprivado,braga).
+
+% - Consultas
+
+consulta(010218,12345,00001,50).
+consulta(200319,69,00005,69).
 
